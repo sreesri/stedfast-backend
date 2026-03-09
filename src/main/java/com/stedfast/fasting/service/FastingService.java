5e37b550dd3457c3024a9870c620e68c3d5a461c
shriram ms <dev.shriram.ms@gmail.com>
2026-03-09 22:52:51 +0530

    feat: Update fasting status to FASTING with current time when no user fasting record exists

--- src/main/java/com/stedfast/fasting/service/FastingService.java
+++ src/main/java/com/stedfast/fasting/service/FastingService.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.ZonedDateTime;
 import java.util.Optional;
 
 @Service
@@ -36,8 +37,8 @@ public class FastingService {
                 } else {
                         return CurrentFastingResponse.builder()
                                         .userId(user.getId())
-                                        .status(FastingStatus.IDLE)
-                                        .startTime(null)
+                                        .status(FastingStatus.FASTING)
+                                        .startTime(ZonedDateTime.now())
                                         .build();
                 }
         }
